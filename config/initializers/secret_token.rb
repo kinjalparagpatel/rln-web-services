2491b7ae0b36d89b998c63d2f39119ad2fd14326
kinjal patel <kinjal.patel@cerner.com>
2013-06-03 11:05:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RlnWebServices::Application.config.secret_token = '9dd14c9ded4c89ebbe8a03f5af0a00047c5be9c7186c209a3aff1bf0376188710182564285c2dbaf7f015fee8f5e68af5033bf73d6f025d64361089b390257dd'
